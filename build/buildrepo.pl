93758b4a35075873d3c378b52789a449f679c5a4
Steve Fryatt <info@stevefryatt.org.uk>
2012-11-11 19:49:25 +0000

    Start to restructure for generating output files.

--- build/buildrepo.pl
+++ build/buildrepo.pl
@@ -37,24 +37,25 @@ use IO::Uncompress::Unzip qw(unzip $UnzipError);
 
 # The root of the repository; must end in /
 
-my $root = "/home/steve/Development/Packaging/Repo/";
+my $root = "/home/steve/Development/Packaging/Repo/software/repo/";
 
 # The package index folder; must end in ;
 
 my $indexes = "pkg/";
 
-# Repository details; index, folder [, folder... ]
+# Repository details; index, catalogue, folder [, folder... ]
 
 my @repos = (
-	['stable',	'stable'],
-	['unstable',	'unstable']);
+	['stable',	'stable.html',		'stable'],
+	['unstable',	'unstable.html',	'unstable']);
 
 foreach my $repo (@repos) {
 	my $index = shift @$repo;
+	my $catalogue = shift @$repo;
 
 	print "Generate repository index " . $index . "\n";
 
-	build_repo($root, $indexes, $index, @$repo);
+	build_repo($root, $indexes, $index, $catalogue, @$repo);
 }
 
 
@@ -66,10 +67,15 @@ foreach my $repo (@repos) {
 # Param $root		The repository root directory.
 # Param $indexes	The index folder for the repository.
 # Param $index		The index file for the repository.
+# Param $catalogue	The catalogue file for the repository.
 # Param @folders	The folders to be scanned for the repository.
 
 sub build_repo {
-	my ($root, $indexes, $index, @folders) = @_;
+	my ($root, $indexes, $index, $catalogue, @folders) = @_;
+
+	open(CATALOGUE, ">".$root.$catalogue) || die "Can't open output file: $!\n";
+
+	print CATALOGUE make_catalogue_header("Repo Name");
 
 	foreach my $folder (@folders) {
 		my $rule = File::Find::Rule->new;
@@ -113,21 +119,28 @@ sub build_repo {
 				print "URL: " . $relative . "\n";
 				print "\n\n";
 
-				print "<h2>" . ((defined($field->{'Package'})) ? $field->{'Package'} : "Untitled") . "</h2>\n\n";
+
+				print CATALOGUE "<img src=\"../images/module.png\" alt=\"\" width=34 height=34 class=\"list-image\">\n\n";
+
+				print CATALOGUE "<h3>" . ((defined($field->{'Package'})) ? $field->{'Package'} : "Untitled") . "</h3>\n\n";
 
 				if (defined($field->{'Description'})) {
 					my $description = $field->{'Description'};
 
 					$description =~ s|\n|</p>\n\n<p>|g;
 
-					print "<p>".$description."</p>\n\n";
+					print CATALOGUE "<p>".$description."</p>\n\n";
 				}
 
-				print "\n\n";
+				print CATALOGUE "\n\n";
 			} else {
 				print "Unzip failed: " . $UnzipError . "\n";
 			}
 		}
+
+	print CATALOGUE make_catalogue_footer();
+
+	close(CATALOGUE);
 	}
 }
 
@@ -180,4 +193,81 @@ sub parse_control {
 	}
 
 	return \%field;
+}
+
+sub make_catalogue_header {
+	my ($name) = @_;
+
+	return
+
+"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\">
+
+<html>
+
+<head>
+<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">
+<link rel=\"stylesheet\" type=\"text/css\" href=\"../../style/base.css\" media=\"screen\">
+<title>RISC OS Software &ndash; Repositories &ndash; Repo</title>
+</head>
+
+<body bgcolor=\"#ffffff\" text=\"#000000\">
+<div id=\"container\">
+<div id=\"header\">
+<h1>Repositories &ndash; Repo</h1>
+</div>
+
+<div id=\"content\">
+
+<p class=\"breadcrumb\">[ <a href=\"../../\" class=\"breadcrumb\">Home</a>
+| <a href=\"../\" class=\"breadcrumb\">RISC OS Software</a>
+| <a href=\"index.html\" class=\"breadcrumb\">Repositories</a>
+| <span class=\"breadcrumb-here\">Repo</span> ]</p>
+
+<p>The pieces of software on this page all falls under the heading of
+&quot;desktop utilities&quot; - adding small extra features or
+functionality to the desktop.  Many of them are relocatable modules that
+can be added to the RISC OS boot sequence, though there are also some
+small applications or even BASIC programs here too.</p>
+
+<h2>Compatibility</h2>
+
+<p>All the software on this page (apart from <cite>Find Icon Bar</cite>,
+which isn't required with the &quot;Nested Wimp&quot;) is RISC OS 4
+compatible and has been tested on RISC OS Select. If a minimum version of
+RISC OS is required, this is indicated next to the download archive; all
+software requires RISC OS 3.1 or better.</p>
+
+<p>With the arrival of RISC OS 5, work is underway to ensure that all the
+software (when applicable) will work on both 26-bit and 32-bit systems.
+Most items are now marked &quot;26/32-bit neutral&quot;; anything tested
+to Castle's requirements will also be marked with the &quot;Iyonix
+OK&quot; logo.</p>
+
+
+";
+}
+
+sub make_catalogue_footer {
+	return
+
+"<p class=\"breadcrumb\">[ <a href=\"../../\" class=\"breadcrumb\">Home</a>
+| <a href=\"../\" class=\"breadcrumb\">RISC OS Software</a>
+| <a href=\"index.html\" class=\"breadcrumb\">Repositories</a>
+| <span class=\"breadcrumb-here\">Repositories</span> ]</p>
+
+</div>
+
+<div id=\"footer\">
+<p><a href=\"http://validator.w3.org/check?uri=referer\"><img src=\"../../images/vh40.gif\" alt=\"Valid HTML 4.0!\" width=88 height=31 border=0></a>&nbsp;
+<a href=\"http://www.riscos.com/\"><img src=\"../../images/roro4x.gif\" alt=\"RISC OS\" width=88 height=31 border=0></a>&nbsp;
+<a href=\"http://www.anybrowser.org/campaign/\"><img src=\"../../images/any.gif\" alt=\"Best veiwed with Any Browser!\" width=81 height=31 border=0></a>&nbsp;
+<a href=\"http://jigsaw.w3.org/css-validator/check/referer\"><img src=\"../../images/vcss.gif\" alt=\"Valid CSS!\" width=88 height=31 border=0></a></p>
+
+<p>Page last updated 5th August, 2007 | Maintained by Steve Fryatt:
+<a href=\"mailto:web\@stevefryatt.org.uk\">web\@stevefryatt.org.uk</a></p>
+</div>
+</div>
+</body>
+</html>
+";
 }
